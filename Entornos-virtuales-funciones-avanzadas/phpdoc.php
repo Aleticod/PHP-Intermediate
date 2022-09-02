9a15278ac59fc6a0ca62e8ed078bea368f870dff
Harvey Unsaac <193837@unsaac.edu.pe>
2022-09-02 06:26:54 +0200

    :memo: Add code with PHPDoc

---
+++ Entornos-virtuales-funciones-avanzadas/phpdoc.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Esta funcion retorna la suma de dos numeros
+ * 
+ * @param int $n1
+ * @param int $n2
+ * @return int
+ */
+function sumar($n1, $n2) {
+    return $n1 + $n2;
+}
+
+$resultador = sumar(1, 2);
+
+echo $resultado;
+
+echo "\n";
